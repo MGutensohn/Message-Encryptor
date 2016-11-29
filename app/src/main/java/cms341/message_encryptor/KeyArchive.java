11e4a7f6421464b26d26200f74181bc243bf6852
michael <mgutensohn@rollins.edu>
2016-11-29 12:52:13 -0500

    started on key archive

---
+++ app/src/main/java/cms341/message_encryptor/KeyArchive.java
@@ -0,0 +1,13 @@
+package cms341.message_encryptor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KeyArchive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_archive);
+    }
+}
