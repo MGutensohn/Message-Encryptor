e0729e9e32ca907050b8c29512ba9f60fe0679d3
michael <mgutensohn@rollins.edu>
2016-12-04 16:36:11 -0500

    KeyGenerator

--- app/src/main/java/cms341/message_encryptor/KeyGenerator.java
+++ app/src/main/java/cms341/message_encryptor/KeyGenerator.java
@@ -7,13 +7,19 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Random;
+import java.util.Scanner;
 
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -32,6 +38,9 @@ public class KeyGenerator extends AppCompatActivity {
         Random random;
         String RandomAudioFileName = "ABCDEFGHIJKLMNOP";
         public static final int RequestPermissionCode = 1;
+        private boolean onClick = false;
+        private boolean permissionsCheck;
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -39,19 +48,24 @@ public class KeyGenerator extends AppCompatActivity {
             setContentView(R.layout.key_generator);
 
             recordButton = (Button) findViewById(R.id.record);
-
+            permissionsCheck = checkPermission();
+            if (!permissionsCheck) {
+                requestPermission();
+            }
             random = new Random();
 
             recordButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
-                    if(checkPermission()) {
+                    onClick = !onClick;
+                if (onClick) {
+                    recordButton.setText(R.string.start);
                         AudioSavePathInDevice =
-                                getFilesDir().getAbsolutePath() + "/" +
-                                        CreateRandomAudioFileName(5) + "AudioRecording.txt";
+                                Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
+                                        CreateRandomAudioFileName(5) + "AudioRecording.3gp";
 
                         MediaRecorderReady();
+                        recordButton.setText(R.string.stop);
 
                         try {
                             mediaRecorder.prepare();
@@ -66,9 +80,12 @@ public class KeyGenerator extends AppCompatActivity {
 
                         Toast.makeText(KeyGenerator.this, "Recording started",
                                 Toast.LENGTH_LONG).show();
-                    } else {
-                        requestPermission();
-                    }
+                } else {
+                    mediaRecorder.stop();
+                    recordButton.setText("Start Recording");
+                    System.err.println("\n\n File: " + AudioSavePathInDevice);
+                    createKey(AudioSavePathInDevice);
+                }
                 }
             });
         }
@@ -128,4 +145,23 @@ public class KeyGenerator extends AppCompatActivity {
             return result == PackageManager.PERMISSION_GRANTED &&
                     result1 == PackageManager.PERMISSION_GRANTED;
          }
+
+        public void createKey(String audioSavePathInDevice) {
+            File file = new File(audioSavePathInDevice);
+            int size = (int) file.length();
+            byte[] bytes = new byte[size];
+            try {
+                BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+                buf.read(bytes, 0, bytes.length);
+                buf.close();
+                System.out.println("\n\n file content: " + Base64.encodeToString(bytes, Base64.DEFAULT));
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
 }
